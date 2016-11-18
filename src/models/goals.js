ddf1b46581bad4f4eb366aa9b5daba8848b17afb
Amanda Free <amanda.free02@gmail.com>
2016-11-18 11:27:21 -0500

    Updating

--- src/models/goals.js
+++ src/models/goals.js
@@ -5,11 +5,11 @@ var mongoose = require('mongoose');
 //goal.name
 //goal.completed
 
-var goalschema = new mongoose.Schema({
+var goalSchema = new mongoose.Schema({
   name: String,
   completed: Boolean
 });
 
-var model = mongoose.model('Goal', goalschema);
+var model = mongoose.model('Goal', goalSchema);
 
 module.exports = model;
